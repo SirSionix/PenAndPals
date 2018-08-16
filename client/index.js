21f551ffe17bf19fb0b24594d9cd03990e781d8f
Elsa <jun-chan@gmx.net>
2018-08-16 12:41:06 +0200

    fixes minor issues

--- client/index.js
+++ client/index.js
@@ -1,29 +1,7 @@
 // fills the dropdown with katagories from the server
 $(document).ready(() => {
 
-    //$('#kat-dropdown').click( () => {
-    $.ajax({
-        type: "GET",
-        url: "http://localhost:3000/kategorien/",
-        success: (data) => {
-            var kategorien = data;//jQuery.parseJSON(data);
-            console.log(data);
-            var dropdown = $('#kat-dropdown-anlegen');
-
-            dropdown.html('');
-            dropdown.append('\'<option value="">Kategorie auswählen</option>\'');
-
-            if (kategorien != '') {
-                $.each(kategorien, (k, v) => {
-                        dropdown.append('<option value="' + v.id + '">' + v.name + '</option>');
-                    }
-                )
-            }
-        }
-    });
-    //});
-
-// fills the dropdown with katagories from the server
+    // fills the dropdown with katagories from the server
     $.ajax({
         type: "GET",
         url: "http://localhost:3000/kategorien/",
@@ -44,6 +22,26 @@ $(document).ready(() => {
         }
     });
 
+// fills the dropdown with katagories from the server
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:3000/kategorien/",
+        success: (data) => {
+            var kategorien = data;//jQuery.parseJSON(data);
+            console.log(data);
+            var dropdown = $('#kat-dropdown-anlegen');
+
+            dropdown.html('');
+            dropdown.append('\'<option value="">Kategorie auswählen</option>\'');
+
+            if (kategorien != '') {
+                $.each(kategorien, (k, v) => {
+                        dropdown.append('<option value="' + v.id + '">' + v.name + '</option>');
+                    }
+                )
+            }
+        }
+    });
 
 // fills the dropdown with systems from the server
     $.ajax({
@@ -65,7 +63,6 @@ $(document).ready(() => {
             }
         }
     });
-//});
 
 // Benutzer anlegen
     $('#BenutzerAnlegenBtn').click(() => {
@@ -74,19 +71,19 @@ $(document).ready(() => {
 
         console.log(name); //# TODO remove
 
-      //  if (name != '' &&   ||   ) { //TODO check for all fields being filled out
+        //  if (name != '' &&   ||   ) { //TODO check for all fields being filled out
 
-            $.ajax({
-                url: 'http://localhost:3000/users/new',
-                type: 'POST',
-                data: {
-                    "name": $('#BenutzernameInputReg').val(),
-                    "email": $('#EmailInputReg').val(),
-                    "password": $('#PassInputReg').val()
-                },
-                dataType: 'json',
-                //TODO now fucking what
-            });
-        })
+        $.ajax({
+            url: 'http://localhost:3000/users/new',
+            type: 'POST',
+            data: {
+                "name": $('#BenutzernameInputReg').val(),
+                "email": $('#EmailInputReg').val(),
+                "password": $('#PassInputReg').val()
+            },
+            dataType: 'json',
+            //TODO now fucking what
+        });
+    })
 
-});
+})
