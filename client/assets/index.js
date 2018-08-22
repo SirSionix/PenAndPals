04f2bf9bb2d6ba0b26516156c9a4ffda3d294521
Elsa <jun-chan@gmx.net>
2018-08-22 18:42:42 +0200

    Add apidoc npm task, refactor index.js

--- client/assets/index.js
+++ client/assets/index.js
@@ -1,193 +1,186 @@
-// fills the dropdown with katagories from the server
 $(document).ready(() => {
 
-    // fills the dropdown with katagories from the server
+    // Menüs mit Daten vom Server befüllen
+    populateCategoriesDropdown();
+    populateSystemsDropdown();
+
+
+    // Benutzer anlegen
+    $('#BenutzerAnlegenBtn').click(createUser);
+
+    // Event anlegen
+    $('#EventAnlegenBtn').click(createEvent);
+
+    // Event suchen
+    $('#EventSuchenBtn').click(searchEvent);
+});
+
+
+
+/**
+ * Fills the dropdown menues with Katagories from the server
+ */
+function populateCategoriesDropdown() {
     $.ajax({
         type: "GET",
         url: "/kategorien/",
         success: (data) => {
-            var kategorien = data;//jQuery.parseJSON(data);
-            console.log(data);
-            var dropdown = $('#kat-dropdown-suchen');
+            var kda = $('#kat-dropdown-anlegen');
+            var kds = $('#kat-dropdown-suchen');
 
-            dropdown.html('');
-            dropdown.append('\'<option value="">Kategorie auswählen</option>\'');
+            kda.html('');
+            kda.append('\'<option value="">Kategorie auswählen</option>\'');
+            kds.html('');
+            kds.append('\'<option value="">Kategorie auswählen</option>\'');
 
-            if (kategorien != '') {
-                $.each(kategorien, (k, v) => {
-                        dropdown.append('<option value="' + v.id + '">' + v.name + '</option>');
+            if (data != '') {
+                $.each(data, (k, v) => {
+                        kda.append('<option value="' + v.id + '">' + v.name + '</option>');
+                        kds.append('<option value="' + v.id + '">' + v.name + '</option>');
                     }
                 )
             }
         }
     });
+}
 
-// fills the dropdown with katagories from the server
-    $.ajax({
-        type: "GET",
-        url: "/kategorien/",
-        success: (data) => {
-            var kategorien = data;//jQuery.parseJSON(data);
-            console.log(data);
-            var dropdown = $('#kat-dropdown-anlegen');
 
-            dropdown.html('');
-            dropdown.append('\'<option value="">Kategorie auswählen</option>\'');
 
-            if (kategorien != '') {
-                $.each(kategorien, (k, v) => {
-                        dropdown.append('<option value="' + v.id + '">' + v.name + '</option>');
-                    }
-                )
-            }
-        }
-    });
-
-// fills the dropdown with systems from the server
+/*
+ * Fills the dropdown menues with systems from the server
+ */
+function populateSystemsDropdown() {
     $.ajax({
         type: "GET",
         url: "/systeme/",
         success: (data) => {
-            var systeme = data;//jQuery.parseJSON(data);
-            console.log(data);
-            var dropdown = $('#sys-dropdown-anlegen');
+            var sda = $('#sys-dropdown-anlegen');
+            var sds = $('#sys-dropdown-suchen');
 
-            dropdown.html('');
-            dropdown.append('\'<option value="">System auswählen</option>\'');
+            sda.html('');
+            sda.append('\'<option value="">System auswählen</option>\'');
+            sds.html('');
+            sds.append('\'<option value="">System auswählen</option>\'');
 
-            if (systeme != '') {
-                $.each(systeme, (k, v) => {
-                        dropdown.append('<option value="' + v.id + '">' + v.name + '</option>');
+            if (data != '') {
+                $.each(data, (k, v) => {
+                        sda.append('<option value="' + v.id + '">' + v.name + '</option>');
+                        sds.append('<option value="' + v.id + '">' + v.name + '</option>');
                     }
                 )
             }
         }
     });
+}
 
-    // fills the dropdown with systems from the server
-    $.ajax({
-        type: "GET",
-        url: "/systeme/",
-        success: (data) => {
-            var systeme = data;//jQuery.parseJSON(data);
-            console.log(data);
-            var dropdown = $('#sys-dropdown-suchen');
 
-            dropdown.html('');
-            dropdown.append('\'<option value="">System auswählen</option>\'');
 
-            if (systeme != '') {
-                $.each(systeme, (k, v) => {
-                        dropdown.append('<option value="' + v.id + '">' + v.name + '</option>');
-                    }
-                )
-            }
-        }
-    });
+/**
+ * Creates a new user
+ */
+function createUser() {
+    var name     = $('#BenutzernameInputReg').val();
+    var email    = $('#EmailInputReg').val();
+    var password = $('#PassInputReg').val();
+    var passwordCheck = $('#PassCheckInputReg').val()
 
-// Benutzer anlegen
-    $('#BenutzerAnlegenBtn').click(() => {
-
-        var name = $('#BenutzernameInputReg').val();
-
-        console.log(name); //# TODO remove
-
-        //  if (name != '' &&   ||   ) { //TODO check for all fields being filled out
+    if (name != '' && email != '' && password != '' && passwordCheck == password) {
+        console.log("test");
 
         $.ajax({
             url: '/users/new',
             type: 'POST',
             data: {
-                "name": $('#BenutzernameInputReg').val(),
-                "email": $('#EmailInputReg').val(),
-                "password": $('#PassInputReg').val()
+                "name": name,
+                "email": email,
+                "password": password
             },
             dataType: 'json',
         });
-    });
-
-// Event anlegen
-    $('#EventAnlegenBtn').click(() => {
-
-        var name = $('#NameInputTA').val();
-        var kat = $('#kat-dropdown-anlegen :selected').text();
-        var ort = $('#OrtInputTA').val();
-        var kontakt = $('#KontaktInputTA').val();
-        var plz = $('#PLZInputTA').val();
-        var sys = $('#sys-dropdown-anlegen :selected').text();
+        console.log("test2");
+    }
+}
 
 
-          if (name != '' && kat != '' && kontakt != '' && plz != '' && sys != '') {
 
-              $.ajax({
-
-                  url: '/events/new',
-                  type: 'POST',
-                  data: {
-                      "name": $('#NameInputTA').val(),
-                      "plz": $('#PLZInputTA').val(),
-                      "ortsname": $('#OrtInputTA').val(),
-                      "kontaktweg": $('#KontaktInputTA').val(),
-                      "kategorieName": $('#kat-dropdown-anlegen :selected').text(),
-                      "systemName": $('#sys-dropdown-anlegen :selected').text(),
-                      "beschreibung": $('#BeschreibungAnlegen').val()
-
-                  },
-                  dataType: 'json',
-              });
-          }
-    });
-
-
-    $('#EventSuchenBtn').click(() => {
-
-        var kat = $('#kat-dropdown-suchen :selected').text();
-        var plz = $('#PLZInputTF').val();
-        var sys = $('#sys-dropdown-suchen :selected').text();
-        var query = {};
-
-        if (kat != '' && kat != 'Kategorie auswählen')
-            query.kategorieName = kat;
-
-        if (plz != '')
-            query.plzshort = plz.substring(0,2);
-
-        if (sys != '' && sys != 'System auswählen')
-            query.systemName = sys;
-
-        var queryString = $.param(query);
-
-
-        $('#results').children().remove();
-
-        $.ajax({
-
-            url: "/events/src" + '?' + queryString,
-            type: "GET",
-            success: (data) => {
-                var eingabe = data;//jQuery.parseJSON(data);
-                console.log(data);
-                var liste = $('#results');
-
-                if (eingabe != '') {
-                    $.each(eingabe, (k, v) => {
-                        $('<div>').addClass('result').append(
-                                $('<hr>'),
-                                $('<p>').text(v.name),
-                                $('<p>').text(v.kategorieName),
-                                $('<p>').text(v.ortsname),
-                                $('<p>').text(v.plz),
-                                $('<p>').text(v.kontaktweg)
-                            ).appendTo(liste);
-                        }
-                    )
-                }
-                // window.location.href = 'Anzeigenansicht.html';
-            }
-        });
-
-    })
-});
 /**
- * Here Be a test
+ * Creates a new event
  */
+function createEvent() {
+    var name    = $('#NameInputTA').val();
+    var plz     = $('#PLZInputTA').val();
+    var ort     = $('#OrtInputTA').val();
+    var kontakt = $('#KontaktInputTA').val();
+    var kat     = $('#kat-dropdown-anlegen :selected').text();
+    var sys     = $('#sys-dropdown-anlegen :selected').text();
+    var beschr  = $('#BeschreibungAnlegen').val();
+    var datum   = $('#DatumAnlegen').val();
+
+    if (name != '' && plz != '' && kontakt != '' && kat != '' && sys != '') {
+        $.ajax({
+            url: '/events/new',
+            type: 'POST',
+            data: {
+                "name": name,
+                "plz": plz,
+                "ortsname": ort,
+                "kontaktweg": kontakt,
+                "kategorieName": kat,
+                "systemName": sys,
+                "beschreibung": beschr,
+                "datum": datum
+            },
+            dataType: 'json',
+        });
+    }
+}
+
+
+
+/**
+ * Searches for an event
+ */
+function searchEvent() {
+    var kat   = $('#kat-dropdown-suchen :selected').text();
+    var plz   = $('#PLZInputTF').val();
+    var sys   = $('#sys-dropdown-suchen :selected').text();
+    var query = {};
+
+    if (kat != '' && kat != 'Kategorie auswählen')
+        query.kategorieName = kat;
+
+    if (plz != '')
+        query.plzshort = plz.substring(0,2);
+
+    if (sys != '' && sys != 'System auswählen')
+        query.systemName = sys;
+
+    var queryString = $.param(query);
+
+
+    // Clear old search results
+    $('#results').children().remove();
+
+    $.ajax({
+        url: "/events/src" + '?' + queryString,
+        type: "GET",
+        success: (data) => {
+            var liste = $('#results');
+
+            if (data != '') {
+                $.each(data, (k, v) => {
+                        $('<div>').addClass('result').append(
+                            $('<hr>'),
+                            $('<p>').text(v.name),
+                            $('<p>').text(v.kategorieName),
+                            $('<p>').text(v.ortsname),
+                            $('<p>').text(v.plz),
+                            $('<p>').text(v.kontaktweg)
+                        ).appendTo(liste);
+                    }
+                )
+            }
+        }
+    });
+}
+
